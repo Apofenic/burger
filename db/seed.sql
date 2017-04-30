13628af6b767db9804546982c32b60f11a1345b5
Anthony Lubrino <anthony1@Anthonys-MacBook-Pro-2.local>
2017-04-29 23:02:09 -0400

    commit 1

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (Burger_name) VALUES ('Big Mac');
